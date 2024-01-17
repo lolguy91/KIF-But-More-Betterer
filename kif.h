238b3d354cfa261cab8f78d2eb2629de1340da82
puffer <kevin@alavik.se>
2024-01-17 08:20:49 +0100

    Update kif.h

--- kif.h
+++ kif.h
@@ -1,10 +1,9 @@
 #ifndef KIF_H_
 #define KIF_H_
 
-#include <limine.h>
 #include <stdint.h>
 #include <display/vga.h> // Change this to your VGA handler, to draw pixels
 
-void draw_image(struct limine_file* image);
+void draw_image(char* rawData, uint64_t startX, uint64_t startY)
 
 #endif // KIF_H_
