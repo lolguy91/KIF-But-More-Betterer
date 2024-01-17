4bae17f11d9a3c1585d4518555ddf2f5131554c1
puffer <kevin@alavik.se>
2024-01-17 18:19:51 +0100

    Update kif.h

--- kif.h
+++ kif.h
@@ -3,10 +3,6 @@
 
 #include <stdint.h>
 
-#define draw_pixel(x, y, r, g, b) plot_pixel(x, y, r, g, b)
-
-// ^^^ Change the plot_pixel function to your draw thing
-
-void draw_image(char* rawData, uint64_t startX, uint64_t startY)
+void draw_image(char* rawData, uint64_t x, uint64_t y);
 
 #endif // KIF_H_
