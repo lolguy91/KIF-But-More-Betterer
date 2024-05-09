2dc5590effb14650856e75653d297aa098305140
lolguy91 <retek1544@gmail.com>
2024-05-09 17:13:04 +0000

    commented the generator code

--- generator.lua
+++ generator.lua
@@ -1,27 +1,46 @@
+-- Generate a random white noise image with the given dimensions
 local function generateWhiteNoise(width,height)
+    -- Create an empty table to hold the pixels
     local pixels = {}
-    local random = math.random
+
+    -- Iterate over all the rows of the image
     for y = 1, height do
+        -- Create an empty table to hold the pixels in this row
         local row = {}
+
+        -- Iterate over all the columns of the image
         for x = 1, width do
-            local r = random(255)
-            local g = random(255)
-            local b = random(255)
-            local a = random(255)
+            -- Generate random values for the RGBA color components
+            local r = math.random(255)
+            local g = math.random(255)
+            local b = math.random(255)
+            local a = math.random(255)
+
+            -- Add the pixel to the row
             table.insert(row,{r,g,b,a})
         end
+
+        -- Add the row to the pixels
         table.insert(pixels,row)
     end
+
+    -- Return the pixels
     return pixels
 end
 
+-- Write a KIFBMB image to the given file with the given dimensions and pixels
 local function writeKIFBMB(fileName,width,height,pixels)
+    -- Open the file
     local file = io.open(fileName, "wb")
+
+    -- Create the header
     local header = {
         magic = 0x69420666,
         width = width,
         height = height,
     }
+
+    -- Calculate the checksum
     local checksum = 0x69 + 0x42 + 0x06 + 0x66
     checksum = checksum + split_and_add_u32(width)
     checksum = checksum + split_and_add_u32(height)
@@ -35,18 +54,28 @@ local function writeKIFBMB(fileName,width,height,pixels)
         end
     end
 
+    -- Set the checksum in the header
     header.checksum = 256 - (checksum % 256)
+
+    -- Write the header
     file:write(string.pack(">IIIB", header.magic, header.width, header.height, header.checksum))
+
+    -- Write the pixels
     for x = 1, width do
         for y = 1, height do
             local pixel = pixels[x][y]
             file:write(string.pack(">BBBB", pixel[1], pixel[2], pixel[3], pixel[4]))
         end
     end
+
+    -- Close the file
     file:close()
 end
+
+-- Split a u32 into 4 bytes and add them
 function split_and_add_u32(val)
     return (val & 0xFF) + ((val >> 8) & 0xFF) + ((val >> 16) & 0xFF) + ((val >> 24) & 0xFF)
 end
 
+-- Generate a random white noise image and write it to a file
 writeKIFBMB("noise.kifbmb", 69, 69, generateWhiteNoise(69, 69))
